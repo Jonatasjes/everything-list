61933e0e168d34dd6aa16ea5fa5da88d91fc8c25
Jonatas Eleoterio Silva <jonatas.eleoterio@outlook.com>
2022-02-12 23:20:10 -0300

    feat: ensure SignUpController return an error if no username is provided

---
+++ backend/src/presentation/controllers/signup/signup.ts
@@ -0,0 +1,12 @@
+import { MissingParamError } from '../../errors/missing-param-error'
+import { Controller } from '../../protocols/controller'
+import { HttpRequest, HttpResponse } from '../../protocols/http'
+
+export class SignUpController implements Controller {
+  handle(httpRequest: HttpRequest): HttpResponse {
+    return {
+      statusCode: 400,
+      body: new MissingParamError('username'),
+    }
+  }
+}
