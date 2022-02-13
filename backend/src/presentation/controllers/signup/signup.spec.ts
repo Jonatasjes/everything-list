8203060ebc57da397774063c8320e98fd16006de
Jonatas Eleoterio Silva <jonatas.eleoterio@outlook.com>
2022-02-13 00:36:53 -0300

    refactor: move httpRequest creation to a factory helper method

--- backend/src/presentation/controllers/signup/signup.spec.ts
+++ backend/src/presentation/controllers/signup/signup.spec.ts
@@ -1,6 +1,24 @@
 import { MissingParamError } from '../../errors/missing-param-error'
 import { badRequest } from '../../helpers/http-helpers'
+import { HttpRequest } from '../../protocols/http'
 import { SignUpController } from './signup'
+
+const makeHttpRequest = (missingField: string): HttpRequest => {
+  const body = {
+    username: 'any_username',
+    name: 'any_name',
+    email: 'any_email',
+    password: 'any_password',
+    passwordConfimation: 'any_passwordConfimation',
+  }
+  if (missingField) {
+    delete body[missingField]
+  }
+
+  return {
+    body,
+  }
+}
 
 interface SutTypes {
   sut: SignUpController
@@ -14,29 +32,13 @@ const makeSut = (): SutTypes => {
 describe('SignUp Controller', () => {
   test('Should return 400 is if no username is provided', () => {
     const { sut } = makeSut()
-    const httpRequest = {
-      body: {
-        name: 'any_name',
-        email: 'any_email',
-        password: 'any_password',
-        passwordConfimation: 'any_passwordConfimation',
-      },
-    }
-    const httpResponse = sut.handle(httpRequest)
+    const httpResponse = sut.handle(makeHttpRequest('username'))
     expect(httpResponse).toEqual(badRequest(new MissingParamError('username')))
   })
 
   test('Should return 400 is if no name is provided', () => {
     const { sut } = makeSut()
-    const httpRequest = {
-      body: {
-        username: 'any_username',
-        email: 'any_email',
-        password: 'any_password',
-        passwordConfimation: 'any_passwordConfimation',
-      },
-    }
-    const httpResponse = sut.handle(httpRequest)
+    const httpResponse = sut.handle(makeHttpRequest('name'))
     expect(httpResponse).toEqual(badRequest(new MissingParamError('name')))
   })
 })
