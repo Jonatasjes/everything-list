66e5d199f0934e2c5507b9a1e1591e8c40f7f33c
Jonatas Eleoterio Silva <jonatas.eleoterio@outlook.com>
2022-02-13 01:06:15 -0300

    feat: ensure SignUpController return an error if no passwordConfirmation is provided

--- backend/src/presentation/controllers/signup/signup.ts
+++ backend/src/presentation/controllers/signup/signup.ts
@@ -5,7 +5,7 @@ import { HttpRequest, HttpResponse } from '../../protocols/http'
 
 export class SignUpController implements Controller {
   handle(httpRequest: HttpRequest): HttpResponse {
-    const requiredFields = ['username', 'name', 'email', 'password', 'passwordConfimation']
+    const requiredFields = ['username', 'name', 'email', 'password', 'passwordConfirmation']
     const missingField = requiredFields.filter(field => !httpRequest.body[field])
     if (missingField[0]) {
       return badRequest(new MissingParamError(missingField[0]))
