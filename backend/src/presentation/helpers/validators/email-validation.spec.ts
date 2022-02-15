43bbceb21fded281d2895e9ea0c5093d41dbdf13
Jonatas Eleoterio Silva <jonatas.eleoterio@outlook.com>
2022-02-14 22:13:22 -0300

    test: ensure EmailValidation return Invalid param error if an invalid email is provided

---
+++ backend/src/presentation/helpers/validators/email-validation.spec.ts
@@ -0,0 +1,37 @@
+import { InvalidParamError } from '../../errors/invalid-param-error'
+import { EmailValidator } from '../../protocols/email-validator'
+import { EmailValidation } from './email-validation'
+
+const makeFakeEmailValidator = (): EmailValidator => {
+  class EmailValidatorStub implements EmailValidator {
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    isValid(email: string): boolean {
+      return true
+    }
+  }
+  return new EmailValidatorStub()
+}
+
+interface SutTypes {
+  sut: EmailValidation
+  emailValidatorStub: EmailValidator
+}
+
+const makeSut = (): SutTypes => {
+  const emailValidatorStub = makeFakeEmailValidator()
+  const sut = new EmailValidation('email', emailValidatorStub)
+
+  return {
+    sut,
+    emailValidatorStub,
+  }
+}
+
+describe('Email Validation', () => {
+  test('Should return Invalid param error if an invalid email is provided', () => {
+    const { sut, emailValidatorStub } = makeSut()
+    jest.spyOn(emailValidatorStub, 'isValid').mockReturnValueOnce(false)
+    const error = sut.validate('invalid_email@mail.com')
+    expect(error).toEqual(new InvalidParamError('email'))
+  })
+})
