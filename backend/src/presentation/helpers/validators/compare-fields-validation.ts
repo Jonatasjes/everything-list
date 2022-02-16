5a8e1bf77c5794cd6ce76aefb558f5467f4b37e7
Jonatas Eleoterio Silva <jonatas.eleoterio@outlook.com>
2022-02-16 19:09:33 -0300

    refactor: remove console.log

--- backend/src/presentation/helpers/validators/compare-fields-validation.ts
+++ backend/src/presentation/helpers/validators/compare-fields-validation.ts
@@ -10,12 +10,6 @@ export class CompareFieldsValidation implements Validation {
   }
 
   validate(input: any): Error {
-    console.log(input)
-    console.log(this.fieldName)
-    console.log(this.fieldNameToCompare)
-    console.log(input[this.fieldName])
-    console.log(input[this.fieldNameToCompare])
-
     if (input[this.fieldName] !== input[this.fieldNameToCompare]) {
       return new InvalidParamError(this.fieldNameToCompare)
     }
