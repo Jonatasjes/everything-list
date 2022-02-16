51d8c435bc764619989508d287c30644f1a72a91
Jonatas Eleoterio Silva <jonatas.eleoterio@outlook.com>
2022-02-16 19:06:09 -0300

    test: ensure CompareFieldsValidation return error if password is not equal to passwordConfirmation

---
+++ backend/src/presentation/helpers/validators/compare-fields-validation.spec.ts
@@ -0,0 +1,40 @@
+import { InvalidParamError } from '../../errors/invalid-param-error'
+import { HttpRequest } from '../../protocols/http'
+import { CompareFieldsValidation } from './compare-fields-validation'
+import { Validation } from './validation'
+
+const makeHttpRequest = (missingField?: string): HttpRequest => {
+  const body = {
+    username: 'any_username',
+    name: 'any_name',
+    email: 'any_email',
+    password: 'any_password',
+    passwordConfirmation: 'any_passwordConfimation',
+  }
+  if (missingField) {
+    delete body[missingField]
+  }
+
+  return {
+    body,
+  }
+}
+
+interface SutTypes {
+  sut: Validation
+}
+
+const makeSut = (): SutTypes => {
+  const sut = new CompareFieldsValidation('password', 'passwordConfirmation')
+  return {
+    sut,
+  }
+}
+
+describe('Compare Fields Validation', () => {
+  test('Should retrurn invalid param error if password is not equal to passwordConfirmation', () => {
+    const { sut } = makeSut()
+    const error = sut.validate(makeHttpRequest().body)
+    expect(error).toEqual(new InvalidParamError('passwordConfirmation'))
+  })
+})
