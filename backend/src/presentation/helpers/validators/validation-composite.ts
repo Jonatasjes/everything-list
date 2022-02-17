06bc1ba664c806adbcb0fb7e8fe0ce8cccbafa09
Jonatas Eleoterio Silva <jonatas.eleoterio@outlook.com>
2022-02-16 21:18:34 -0300

    feat: ensure SinupController return 400 is CompareFieldValidation throws

--- backend/src/presentation/helpers/validators/validation-composite.ts
+++ backend/src/presentation/helpers/validators/validation-composite.ts
@@ -3,9 +3,11 @@ import { Validation } from './validation'
 export class ValidationComposite implements Validation {
   private readonly requiredFieldsValidation: Validation
   private readonly emailValidation: Validation
-  constructor(requiredFieldsValidation: Validation, emailValidation: Validation) {
+  private readonly compareFieldsValidation: Validation
+  constructor(requiredFieldsValidation: Validation, emailValidation: Validation, compareFieldsValidation: Validation) {
     this.requiredFieldsValidation = requiredFieldsValidation
     this.emailValidation = emailValidation
+    this.compareFieldsValidation = compareFieldsValidation
   }
   validate(input: any): Error {
     const errorField = this.requiredFieldsValidation.validate(input)
@@ -15,6 +17,10 @@ export class ValidationComposite implements Validation {
     const errorEmail = this.emailValidation.validate(input.email)
     if (errorEmail) {
       return errorEmail
+    }
+    const errorCompare = this.compareFieldsValidation.validate(input)
+    if (errorCompare) {
+      return errorCompare
     }
   }
 }
