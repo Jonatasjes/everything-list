5cb53d6b54a3feace03e604b18595cbe61c0c659
Jonatas Eleoterio Silva <jonatas.eleoterio@outlook.com>
2022-02-21 22:30:50 -0300

    test: ensure throws if addAccountRepoditory throws

--- backend/src/data/usecases/add-account/db-add-account.spec.ts
+++ backend/src/data/usecases/add-account/db-add-account.spec.ts
@@ -90,4 +90,13 @@ describe('DbAddAccount Usecase', () => {
       password: 'hashed_password',
     })
   })
+
+  test('Should throws if addAccountRepository throws', async () => {
+    const { sut, addAccountRepositoryStub } = makeSut()
+    jest
+      .spyOn(addAccountRepositoryStub, 'add')
+      .mockReturnValueOnce(new Promise((resolve, reject) => reject(new Error())))
+    const promise = sut.add(makeAccountData())
+    await expect(promise).rejects.toThrow()
+  })
 })
